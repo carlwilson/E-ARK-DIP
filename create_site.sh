9153a34758b5294b5f3c223820f6c1b817bc6b40
Carl Wilson <carl@openpreservation.org>
2025-08-20 10:08:41 +0000

    FIX: Update workflow actions and improvements
    - [`./create_site.sh`](./create_site.sh) updated to reflect Asciidoctor publishing workflow;
    - removed separate PDF publication step from Actions workflow;
    - update GitHub actions to latest versions; and
    - converted project and `specification` folder `README`s to Asciidoc.

--- create_site.sh
+++ create_site.sh
@@ -4,38 +4,34 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR" || exit
 
 echo " - Cleaning up site directory and copying spec-publisher site..."
-git clean -f specification/ 
-
+git clean -f "$SCRIPT_DIR/specification/"
+git clean -f "$SCRIPT_DIR/site/"
 
 if [ -d _site ]
 then
   echo " - Removing old _site directory contents"
-  rm -rf _site/*
+  rm -rf "$SCRIPT_DIR/_site/*"
 fi
 
 echo " - copying files to site directory..."
 # Copy spec-publisher artifacts to the site
-cp -rf spec-publisher/site/* spec-publisher/res/md/figs site/
+cp -rf "$SCRIPT_DIR/spec-publisher/site/"* "$SCRIPT_DIR/spec-publisher/res/md/figs" "$SCRIPT_DIR/site/"
 # Copy remaining project collaterel to the site
-cp -rf profile archive examples specification/media site/
+cp -rf "$SCRIPT_DIR/profile" "$SCRIPT_DIR/archive" "$SCRIPT_DIR/examples" "$SCRIPT_DIR/specification/media" "$SCRIPT_DIR/site/"
 
 echo " - spec-publisher: generating requirement tables, appendices, etc."
 mvn package -f spec-publisher/pom.xml
-java -jar ./spec-publisher/target/mets-profile-processor-0.1.0-SNAPSHOT.jar -f ./specification.yaml -o doc/site profile/E-ARK-DIP-v2-2-0.xml
+java -jar "$SCRIPT_DIR/spec-publisher/target/mets-profile-processor-0.2.0-SNAPSHOT.jar" -f "$SCRIPT_DIR/specification.yaml" -o "$SCRIPT_DIR/specification" "$SCRIPT_DIR/profile/E-ARK-DIP-v2-2-0.xml"
 
-echo " - MARKDOWN-PP: generating site page with TOC..."
-cd doc/site || exit
-bash "$SCRIPT_DIR/spec-publisher/scripts/create-venv.sh"
-command -v markdown-pp >/dev/null 2>&1 || {
-  tmpdir=$(dirname "$(mktemp -u)")
-  source "$tmpdir/.venv-markdown/bin/activate"
-}
-markdown-pp body.md -o body_toc.md
+echo " - Copying spec-publisher collateral to specification directory."
+cp -rf "$SCRIPT_DIR/spec-publisher/res/md/common-intro.adoc" "$SCRIPT_DIR/spec-publisher/res/md/figs" "$SCRIPT_DIR/specification/"
 
-echo " - MARKDOWN-PP: Processing postface markdown"
-cd "$SCRIPT_DIR/specification/postface/" || exit
-markdown-pp postface.md -o "$SCRIPT_DIR/doc/site/postface.md" -e tableofcontents
+echo " - Generating site HTML with asciidoctor."
+asciidoctor -a linkcss -a copycss -e -o - "$SCRIPT_DIR/specification/eark-dip.adoc" >> "$SCRIPT_DIR/site/index.html"
 
-echo " - MARKDOWN-PP: generating final site index markdown in site/index.md"
-cd "$SCRIPT_DIR/doc/site" || exit
-markdown-pp SITE.md -o ../../site/index.md
+echo " - Generating site PDF with asciidoctor."
+asciidoctor-pdf -o site/pdf/eark-dip.pdf specification/eark-dip.adoc
+
+echo " - Cleaning up site directory and copying spec-publisher site..."
+git clean -f "$SCRIPT_DIR/specification/"
+git clean -f "$SCRIPT_DIR/site/"
